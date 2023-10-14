7b23afcd69f348151d00ba7c2361cc9700cc17d7
MartinNo81 <rcdm@seznam.cz>
2023-10-14 14:49:58 +0200

    Secondary table. Europe data (country, GDP, population). CZE data has been excluded.

---
+++ t_{martin}_{novak}_project_SQL_secondary_final.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE TABLE t_martin_novak_project_SQL_secondary_final AS
+SELECT
+	c.country ,
+	c.capital_city ,
+	e.population ,
+	e.`year` ,
+	e.GDP 
+FROM countries c
+LEFT JOIN economies e 
+	ON lower(c.country) = lower(e.country)  
+WHERE 
+	lower(c.continent) = 'europe' 
+	AND e.`year` >= 2006
+	AND e.`year` <= 2018
+	AND lower(c.country) NOT LIKE 'czech%';
