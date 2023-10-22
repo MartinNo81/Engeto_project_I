8beb323e9ba22d577e0a1f704a49509f4628d2a6
MartinNo81 <rcdm@seznam.cz>
2023-10-22 11:12:11 +0200

    SQL1 Corrections: ','  All_industry,

--- {martin}_{novak}_SQL2.sql
+++ {martin}_{novak}_SQL2.sql
@@ -1,14 +1,13 @@
 SELECT 	
 	`year`,
-	name ,
+	name,
 	round((monthly_salary / price ), 2) AS food_index,
-	price ,
-	`_value` ,
-	price_unit ,
-	IF(name LIKE 'Mléko%',1,0) AS flag_milk,
-	IF(name LIKE 'Chléb%',1,0) AS flag_bread
+	price,
+	`_value`,
+	price_unit,
+	IF(name LIKE 'Mléko%' OR name LIKE 'Chléb%',1,0) AS flag
 FROM t_martin_novak_project_sql_primary_final tmnpspf 
 WHERE 
-	`year`= 2018 AND industry = '_All_in_one'
-	OR `year`= 2006 AND industry = '_All_in_one'
-HAVING flag_milk = 1 OR flag_bread = 1
+	`year`= 2018 AND industry = '_All_industry'
+	OR `year`= 2006 AND industry = '_All_industry'
+HAVING flag=1
