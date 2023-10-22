495d9de326be37e958f309862a5dbb552fc230d2
MartinNo81 <rcdm@seznam.cz>
2023-10-22 11:08:28 +0200

    SQL1 Corrections:BETWEEN ',' '_0' '_1' All_industry, scaling of salary changing,

--- {martin}_{novak}_SQL1.sql
+++ {martin}_{novak}_SQL1.sql
@@ -1,36 +1,40 @@
 WITH t_year_0 AS (
 	SELECT 
-		`year` AS year0,
-		avg(monthly_salary) AS salary0 ,
-		industry AS industry0
-	FROM t_martin_novak_project_SQL_primary_final t
-	WHERE industry != '_All_in_one'
-	GROUP BY `year` ,industry
+		`year` AS year_0,
+		monthly_salary AS salary_0,
+		industry AS industry_0
+	FROM t_martin_novak_project_SQL_primary_final t_0
+	WHERE industry != '_All_industry'
+	GROUP BY year_0, salary_0, industry_0
 ),
 t_year_1 AS (
 	SELECT 
-		`year`+ 1 AS year1,
-		avg(monthly_salary) AS salary1 ,
-		industry AS industry1
-	FROM t_martin_novak_project_SQL_primary_final t1
-	WHERE industry != '_All_in_one'
-	GROUP BY `year` ,industry	
+		`year`+ 1 AS year_1,
+		monthly_salary AS salary_1 ,
+		industry AS industry_1
+	FROM t_martin_novak_project_SQL_primary_final t_1
+	WHERE industry != '_All_industry'
+	GROUP BY year_1, salary_1, industry_1	
 )
 SELECT 
-	year0,
-	salary0,
-	industry0,
-	year1 -1,
-	salary1,
-	industry1,
-	IF(salary0 > salary1,1,0) AS flag
+	year_0,
+	salary_0,
+	industry_0,
+	year_1 -1,
+	salary_1,
+	industry_1,
+	CASE 
+		WHEN salary_1 - salary_0 BETWEEN 0 AND 100 THEN 'less then 0100'
+		WHEN salary_1 - salary_0 BETWEEN 101 AND 500 THEN 'less then 0500'
+		WHEN salary_1 - salary_0 BETWEEN 501 AND 1000 THEN 'less then 1000'
+		WHEN salary_1 - salary_0 > 1000 THEN 'more then 1000'
+		ELSE NULL 
+	END AS decrease_salary
 FROM t_year_0 t
 INNER JOIN t_year_1 t1
-	ON year0 = year1 AND industry0 = industry1
-ORDER BY flag ASC
-LIMIT 25
-
-
+	ON year_0 = year_1 AND industry_0 = industry_1
+ORDER BY decrease_salary DESC
+LIMIT 25;
 
 
 
