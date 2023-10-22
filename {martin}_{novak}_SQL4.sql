507fad408f55beb4df3129abe52ede7ad7c7f302
MartinNo81 <rcdm@seznam.cz>
2023-10-22 11:26:36 +0200

    SQL4 Corrections: ','  '_' All_industry,

--- {martin}_{novak}_SQL4.sql
+++ {martin}_{novak}_SQL4.sql
@@ -1,31 +1,31 @@
 WITH t_year_0 AS (
 		SELECT
-			monthly_salary AS monthly_salary0,
-			industry AS industry0,
-			`year` AS year0,
-			avg(price) AS price0
-		FROM t_martin_novak_project_SQL_primary_final t0
-		WHERE industry = '_All_in_one'
-		GROUP BY monthly_salary0 , industry0 ,year0 
+			monthly_salary AS monthly_salary_0,
+			industry AS industry_0,
+			`year` AS year_0,
+			avg(price) AS price_0
+		FROM t_martin_novak_project_SQL_primary_final t_0
+		WHERE industry = '_All_industry'
+		GROUP BY monthly_salary_0 , industry_0 ,year_0 
 ),
 t_year_1 AS (
 		SELECT
-			monthly_salary AS monthly_salary1,
-			industry AS industry1,
-			`year`+1 AS year1,
-			avg(price) AS price1
-		FROM t_martin_novak_project_SQL_primary_final t1
-		WHERE industry = '_All_in_one'
-		GROUP BY monthly_salary1 , industry1 ,year1 
+			monthly_salary AS monthly_salary_1,
+			industry AS industry_1,
+			`year`+1 AS year_1,
+			avg(price) AS price_1
+		FROM t_martin_novak_project_SQL_primary_final t_1
+		WHERE industry = '_All_industry'
+		GROUP BY monthly_salary_1, industry_1, year_1 
 )
 SELECT
-	concat(year1-1,'-->',year0) AS period,
-	round((((price0 - price1)/price1)*100),2) AS year_increase_price_pct,
-	round((((monthly_salary0 - monthly_salary1)/monthly_salary1)*100),2) AS year_increase_salary_pct,
-	round((((price0 - price1)/price1)*100) - (((monthly_salary0 - monthly_salary1)/monthly_salary1)*100),2) AS difference 
+	concat(year_1-1,'-->',year_0) AS period,
+	round((((price_0 - price_1)/price_1)*100),2) AS year_increase_price_pct,
+	round((((monthly_salary_0 - monthly_salary_1)/monthly_salary_1)*100),2) AS year_increase_salary_pct,
+	round((((price_0 - price_1)/price_1)*100) - (((monthly_salary_0 - monthly_salary_1)/monthly_salary_1)*100),2) AS difference 
 FROM t_year_0 t0
 INNER JOIN t_year_1 t1
-	ON year0 = year1
+	ON year_0 = year_1
 ORDER BY period 
 
 
