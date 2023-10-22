62b104947a2eb5ca3d59f1caba496c3324f004f1
MartinNo81 <rcdm@seznam.cz>
2023-10-22 11:21:28 +0200

    SQL3 Corrections: ','  '_'

--- {martin}_{novak}_SQL3.sql
+++ {martin}_{novak}_SQL3.sql
@@ -1,29 +1,28 @@
 WITH t_year_0 AS (
 	SELECT 
-		`year` AS year0,
-		avg(price) AS price0,
-		name AS name0 
-	FROM t_martin_novak_project_SQL_primary_final t0
-	GROUP BY `year`, name
+		`year` AS year_0,
+		price AS price_0,
+		name AS name_0 
+	FROM t_martin_novak_project_SQL_primary_final t_0
+	GROUP BY year_0, price_0, name_0 
 ),
 t_year_1 AS (
 	SELECT 
-		`year`+ 1 AS year1,
-		avg(price) AS price1,
-		name AS name1 
-	FROM t_martin_novak_project_SQL_primary_final t1
-	GROUP BY `year`, name
+		`year`+ 1 AS year_1,
+		price AS price_1,
+		name AS name_1 
+	FROM t_martin_novak_project_SQL_primary_final t_1
+	GROUP BY year_1, price_1, name_1 
 )
 SELECT 
-	name1 AS product,
-	round(avg(((price0 - price1)/price1)*100),2) AS average_year_percentage_increase
-FROM t_year_0 t0
-INNER JOIN t_year_1 t1
-	ON year0 = year1 AND name0 = name1	
-GROUP BY name1
-ORDER BY average_year_percentage_increase DESC ; 
+	name_1 AS product,
+	round(avg(((price_0 - price_1)/price_1)*100),2) AS average_year_percentage_increase
+FROM t_year_0 t_0
+INNER JOIN t_year_1 t_1
+	ON year_0 = year_1 AND name_0 = name_1	
+GROUP BY name_1
+ORDER BY average_year_percentage_increase DESC; 
 
 
 	
-
 	
