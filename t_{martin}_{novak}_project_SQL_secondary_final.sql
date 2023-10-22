c7a7fa7dd66bae79b4e5bc274e562402095e4362
MartinNo81 <rcdm@seznam.cz>
2023-10-22 10:49:24 +0200

    Secondary table. Corrections:BETWEEN, ','

--- t_{martin}_{novak}_project_SQL_secondary_final.sql
+++ t_{martin}_{novak}_project_SQL_secondary_final.sql
@@ -1,16 +1,14 @@
 CREATE OR REPLACE TABLE t_martin_novak_project_SQL_secondary_final AS
-
 SELECT
-	c.country ,
-	c.capital_city ,
-	e.population ,
-	e.`year` ,
-	round(e.GDP,0) AS GDP 
+	c.country,
+	e.population,
+	e.gini,
+	round(e.GDP,0) AS GDP,
+	e.`year` 
 FROM countries c
 LEFT JOIN economies e 
 	ON lower(c.country) = lower(e.country)  
 WHERE 
 	lower(c.continent) = 'europe' 
-	AND e.`year` >= 2006
-	AND e.`year` <= 2018
-	AND lower(c.country) NOT LIKE 'czech%' ;
+	AND e.`year` BETWEEN 2006 AND 2018
+	AND lower(c.country) NOT LIKE 'czech%';
